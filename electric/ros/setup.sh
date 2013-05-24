656524a59fe222acf0ad83f21215301a0f52a6d5
Sean Greenslade <zootboysean@gmail.com>
2013-05-24 18:37:02 -0400

    Removed old electric packages.

--- electric/ros/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-export ROS_ROOT=/opt/ros/electric/ros
-export PATH=${ROS_ROOT}/bin:${PATH}
-export PYTHONPATH=${ROS_ROOT}/core/roslib/src:${PYTHONPATH}
-export ROS_PACKAGE_PATH=/opt/ros/electric/stacks
-if [ ! "$ROS_MASTER_URI" ] ; then export ROS_MASTER_URI=http://localhost:11311 ; fi
