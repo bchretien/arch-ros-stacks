61f216294c32aca3ff0c1c8e24d6f4435bb1cb6d
Benjamin Chrétien <chretien@lirmm.fr>
2015-06-01 15:32:49 +0900

    jade: add ros package and its dependencies.

---
+++ jade/clean.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Jade directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Remove build directories
+for dir in $(find $DIR -maxdepth 2 -mindepth 2 -type d)
+do
+    rm -rf $dir
+done
+
+# Remove generated packages
+for f in $(find $DIR -maxdepth 2 -mindepth 2 -type f \
+           -name "*.pkg.tar.xz" -or -name "*.src.tar.gz")
+do
+    rm $f
+done
