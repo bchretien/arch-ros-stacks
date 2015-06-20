3e79da845f76db92a9a9ca09521005a7f6710221
Benjamin Chrétien <chretien@lirmm.fr>
2015-06-20 18:03:03 +0900

    to_aur4.sh: add push of last commit

--- to_aur4.sh
+++ to_aur4.sh
@@ -3,16 +3,110 @@
 function usage() {
   echo "Push ROS-related package modifications to the AUR (v4)"
   echo ""
-  echo "Usage 1: $0 <ROS distribution> <ROS package name>"
-  echo "Example: $0 indigo desktop_full"
+  echo "Usage 1: push a specific ROS package for a given ROS" \
+       "distribution"
   echo ""
-  echo "Usage 2: $0 dependency <package name>"
-  echo "Example: $0 dependency python2-empy"
+  echo "  Command: $0 <ROS distribution> <ROS package name>"
+  echo "  Example: $0 indigo desktop_full"
+  echo ""
+  echo ""
+  echo "Usage 2: push a specific ROS dependency package"
+  echo ""
+  echo "  Command: $0 dependency <package name>"
+  echo "  Example: $0 dependency python2-empy"
+  echo ""
+  echo ""
+  echo "Usage 3: push the packages from the last commit"
+  echo ""
+  echo "  Command: $0 commit"
   echo ""
   echo "Copyright 2015 (c) Benjamin Chrétien <chretien+aur@lirmm.fr>"
-  echo "Released under dual GPL license."
+  echo "Released under GPL license."
 }
 
+
+function newPackage() {
+  local package_dir=$1
+  local package_name=$2
+  local branch_name=$3
+  local git_repo=$4
+
+  # Create subtree and run mksrcinfo in it
+  git subtree split --prefix="${package_dir}" -b ${branch_name}
+  squashCheck ${package_name} ${branch_name}
+
+  git stash
+  git filter-branch -f --tree-filter "mksrcinfo" -- ${branch_name}
+  git stash pop
+
+  # Setup repo if the package does not exist
+  ssh -p${AUR4PORT} ${AUR4USER}@${AUR4HOST} setup-repo "${package_name}" || \
+    echo "Failed to setup-repo ${package_name}... Maybe it already exists?"
+
+  # Push subtree to AUR4
+  git push "ssh+git://${git_repo}" "${branch_name}:master" || \
+    echo "git push to ${package_name}.git branch master failed. Maybe repo isn't empty?"
+
+  # Clean branch
+  git branch -D ${branch_name}
+}
+
+function updatePackage() {
+  local package_dir=$1
+  local package_name=$2
+  local branch_name=$3
+  local git_repo=$4
+
+  # Fetch remote from AUR4
+  git fetch "ssh+git://${git_repo}" || \
+    echo "git fetch of ${package_name}.git failed. Maybe package doesn't exist?"
+
+  # Pull from AUR4
+  # FIXME: leads to merge because of remote .SRCINFO
+  #git subtree pull --prefix="${subdir}" "ssh+git://${git_repo}" ${branch_name} || \
+  #  echo "git pull of ${full_name}.git failed. Maybe package doesn't exist?"
+
+  # Create subtree and run mksrcinfo in it
+  git subtree split --prefix="${package_dir}" -b ${branch_name}
+  squashCheck ${package_name} ${branch_name}
+  git stash
+  git filter-branch -f --tree-filter "mksrcinfo" -- ${branch_name}
+  git stash pop
+
+  # Push to AUR4
+  git push "ssh+git://${git_repo}" "${branch_name}:master" || \
+    echo "git push of ${package_name}.git failed. Maybe package doesn't exist?"
+
+  # Clean branch
+  git branch -D ${branch_name}
+}
+
+function processPackage() {
+  local package_dir=$1
+  local package_name=$2
+
+  # Unique branch name for subtree
+  local branch_name="aur4/${package_name}"
+
+  # Git repository in the AUR
+  local git_repo="${AUR4USER}@${AUR4HOST}:${AUR4PORT}/${package_name}.git/"
+
+  # Check that a PKGBUILD exists
+  if [ ! -f "${package_dir}/PKGBUILD" ]; then
+    echo "PKGBUILD not found in ${package_dir}"
+    exit 1
+  fi
+
+  # Check if the package already exists
+  if ! $(ssh -p${AUR4PORT} ${AUR4USER}@${AUR4HOST} list-repos | \
+         grep -q "${package_name}"); then
+    # Create new AUR package
+    newPackage "$@" ${branch_name} ${git_repo}
+  else
+    # Update existing AUR package
+    updatePackage "$@" ${branch_name} ${git_repo}
+  fi
+}
 
 # Some PKGBUILDs were not valid when added to arch-ros-stacks (*cough*
 # ros-build-tools *cough*), so our git filter-branch here will lead to invalid
@@ -79,23 +173,25 @@ AUR4HOST=${AUR4HOST:-aur4.archlinux.org}
 AUR4USER=${AUR4USER:-aur}
 AUR4PORT=${AUR4PORT:-22}
 
-# ROS distribution
-DISTRIB=${1:-}
+# Query or ROS distribution
+USER_QUERY=${1:-}
 
 # ROS package name (e.g. desktop_full)
 LOCAL_NAME=${2:-}
 
-if [[ -z "${DISTRIB}" || -z "${LOCAL_NAME}" ]]; then
+if [[ "${USER_QUERY}" == "dependency" ]]; then
+  query_type="dependency"
+elif [[ "${USER_QUERY}" == "commit" ]]; then
+  query_type="commit"
+elif [[ "${USER_QUERY}" == "indigo" || "${USER_QUERY}" == "jade" ]]; then
+  query_type="ros"
+else
   usage
   exit 1
 fi
 
-if [[ "${DISTRIB}" == "dependency" ]]; then
-  is_ros_package="false"
-elif [[ "${DISTRIB}" == "indigo" || "${DISTRIB}" == "jade" ]]; then
-  is_ros_package="true"
-else
-  echo "ROS distribution should be \"indigo\" or \"jade\""
+if [[ "${query_type}" != "commit" && -z "${LOCAL_NAME}" ]]; then
+  usage
   exit 1
 fi
 
@@ -103,80 +199,47 @@ fi
 arch_ros_stacks_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 pushd ${arch_ros_stacks_dir}
 
-if [[ "${is_ros_package}" == "true" ]]; then
+set -x
+
+if [[ "${query_type}" == "ros" ]]; then
+  # ROS distribution
+  distrib=${USER_QUERY}
+
   # Subdirectory in arch-ros-stacks
-  subdir="${DISTRIB}/${LOCAL_NAME}"
+  subdir="${distrib}/${LOCAL_NAME}"
 
   # Full AUR package name
-  full_name="ros-${DISTRIB}-${LOCAL_NAME//_/-}"
-else
+  full_name="ros-${distrib}-${LOCAL_NAME//_/-}"
+
+  # Process package
+  processPackage ${subdir} ${full_name}
+elif [[ "${query_type}" == "dependency" ]]; then
   # Subdirectory in arch-ros-stacks
   subdir="dependencies/${LOCAL_NAME}"
 
   # Full AUR package name
   full_name="${LOCAL_NAME}"
-fi
 
-# Check that a PKGBUILD exists
-full_pkgbuild_path="${arch_ros_stacks_dir}/${subdir}/PKGBUILD"
-if [ ! -f "${full_pkgbuild_path}" ]; then
-  echo "PKGBUILD not found in ${arch_ros_stacks_dir}/${subdir}"
-  exit 1
-fi
+  # Process package
+  processPackage ${subdir} ${full_name}
+elif [[ "${query_type}" == "commit" ]]; then
+  # TODO
+  updated_pkgbuilds=$(git diff-tree --no-commit-id --name-only -r HEAD | grep PKGBUILD)
+  for p in ${updated_pkgbuilds}; do
+    subdir=$(dirname ${p})
+    first_subdir=${subdir%/*}
+    if [[ "${first_subdir}" == "dependencies" ]]; then
+      full_name=$(basename ${subdir})
+    elif [[ "${first_subdir}" == "indigo" || \
+            "${first_subdir}" == "jade" ]]; then
+      raw_name=$(basename ${subdir})
+      full_name="ros-${first_subdir}-${raw_name//_/-}"
+    fi
 
-# Unique branch name for subtree
-branch_name="aur4/${full_name}"
-
-# Git repository in the AUR
-git_repo="${AUR4USER}@${AUR4HOST}:${AUR4PORT}/${full_name}.git/"
-
-set -x
-
-# Check if the package already exists
-if ! $(ssh -p${AUR4PORT} ${AUR4USER}@${AUR4HOST} list-repos | grep -q "${full_name}"); then
-  # Create subtree and run mksrcinfo in it
-  git subtree split --prefix="${subdir}" -b ${branch_name}
-  squashCheck ${full_name} ${branch_name}
-
-  git stash
-  git filter-branch -f --tree-filter "mksrcinfo" -- ${branch_name}
-  git stash pop
-
-  # Setup repo if the package does not exist
-  ssh -p${AUR4PORT} ${AUR4USER}@${AUR4HOST} setup-repo "${full_name}" || \
-    echo "Failed to setup-repo ${full_name}... Maybe it already exists?"
-
-  # Push subtree to AUR4
-  git push "ssh+git://${git_repo}" "${branch_name}:master" || \
-    echo "git push to ${full_name}.git branch master failed. Maybe repo isn't empty?"
-
-  # Clean branch
-  git branch -D ${branch_name}
-
-# If package already exists
-else
-  # Fetch remote from AUR4
-  git fetch "ssh+git://${git_repo}" || \
-    echo "git fetch of ${full_name}.git failed. Maybe package doesn't exist?"
-
-  # Pull from AUR4
-  # FIXME: leads to merge because of remote .SRCINFO
-  #git subtree pull --prefix="${subdir}" "ssh+git://${git_repo}" ${branch_name} || \
-  #  echo "git pull of ${full_name}.git failed. Maybe package doesn't exist?"
-
-  # Create subtree and run mksrcinfo in it
-  git subtree split --prefix="${subdir}" -b ${branch_name}
-  squashCheck ${full_name} ${branch_name}
-  git stash
-  git filter-branch -f --tree-filter "mksrcinfo" -- ${branch_name}
-  git stash pop
-
-  # Push to AUR4
-  git push "ssh+git://${git_repo}" "${branch_name}:master" || \
-    echo "git push of ${full_name}.git failed. Maybe package doesn't exist?"
-
-  # Clean branch
-  git branch -D ${branch_name}
+    # Process package
+    processPackage ${subdir} ${full_name}
+  done
+  exit 2
 fi
 
 popd
