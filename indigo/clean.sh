cb381e8c2479016f513cf7db903e114102805a8a
Benjamin Chrétien <chretien.b@gmail.com>
2016-02-09 21:08:46 +0900

    indigo: update clean.sh

--- indigo/clean.sh
+++ indigo/clean.sh
@@ -3,15 +3,17 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Remove build directories
-for dir in $(find $DIR -maxdepth 2 -mindepth 2 -type d)
+for dir in $(find ${DIR} -maxdepth 2 -mindepth 2 -type d)
 do
-    rm -rf $dir
+  echo "Cleaning ${dir}"
+  rm -rI ${dir}
 done
 
 # Remove generated packages
-for f in $(find $DIR -maxdepth 2 -mindepth 2 -type f \
+for f in $(find ${DIR} -maxdepth 2 -mindepth 2 -type f \
            -name "*.pkg.tar.xz" -or -name "*.src.tar.gz" \
-           -or -regextype posix-egrep -regex ".*/[0-9.-\_]+\.tar\.gz")
+           -or -regextype posix-egrep -regex "${DIR}/.*/[0-9\_-\.]+\.tar\.gz")
 do
-    rm $f
+  echo "Cleaning ${f}"
+  rm -I ${f}
 done
